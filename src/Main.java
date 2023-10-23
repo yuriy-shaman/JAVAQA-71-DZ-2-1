df972a5a531d8aaab37c4ee46894b5442b5b7f0f
YuriS <yuri-s@list.ru>
2023-10-23 13:23:10 +0300

    Initional commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 5_025;
+        int miles = price / 20;
+
+        System.out.println(" Ваши бонусы: " + miles );
+
+    }
+}
